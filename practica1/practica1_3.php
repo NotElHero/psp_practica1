aadfa90ff60058df451bdbfa34edd70748c280b8
Javier Villacorta <javier.vilgar@educa.jcyl.es>
2022-10-11 11:37:28 +0200

    v1

--- practica1/practica1_3.php
+++ practica1/practica1_3.php
@@ -1,3 +1,73 @@
 <?php
+    $carta1 = rand(1, 4);
+    $carta2 = rand(1, 4);
+    $carta3 = rand(1, 4);
+    $carta4 = rand(1, 4);
 
+    $rand1 = rand(1, 12);
+    $rand2 = rand(1, 12);
+    $rand3 = rand(1, 12);
+    $rand4 = rand(1, 12);
+
+    for ($i=1; $i < 5; $i++) { 
+        $rand = rand(1, 12);
+        switch ($carta1) {
+            case 1:
+                echo "<img src='baraja/Bastos/" +'$rand'+".jpg'>";
+                break;
+            case 2:
+                echo "<img src='baraja/Copas/" +'$rand'+".jpg'>";
+                break;
+            case 3:
+                echo "<img src='babraja/Espadas/" +'$rand'+".jpg'>";
+                break;
+            case 4:
+                echo "<img src='baraja/Oros/" +'$rand'+".jpg'>";
+                break;
+        }
+    }
+
+    
+    // switch ($carta2) {
+    //     case 1:
+    //         echo "<img src='baraja/Bastos/" +"$rand1"+".jpg'>";
+    //         break;
+    //     case 2:
+    //         echo "<img src='baraja/Copas/" +"$rand2"+".jpg'>";
+    //         break;
+    //     case 3:
+    //         echo "<img src='babraja/Espadas/" +"$rand3"+".jpg'>";
+    //         break;
+    //     case 4:
+    //         echo "<img src='baraja/Oros/" +"$rand4"+".jpg'>";
+    //         break;
+    // }
+    // switch ($carta3) {
+    //     case 1:
+    //         echo "<img src='baraja/Bastos/" +"$rand1"+".jpg'>";
+    //         break;
+    //     case 2:
+    //         echo "<img src='baraja/Copas/" +"$rand2"+".jpg'>";
+    //         break;
+    //     case 3:
+    //         echo "<img src='babraja/Espadas/" +"$rand3"+".jpg'>";
+    //         break;
+    //     case 4:
+    //         echo "<img src='baraja/Oros/" +"$rand4"+".jpg'>";
+    //         break;
+    // }
+    // switch ($carta4) {
+    //     case 1:
+    //         echo "<img src='baraja/Bastos/" +"$rand1"+".jpg'>";
+    //         break;
+    //     case 2:
+    //         echo "<img src='baraja/Copas/" +"$rand2"+".jpg'>";
+    //         break;
+    //     case 3:
+    //         echo "<img src='babraja/Espadas/" +"$rand3"+".jpg'>";
+    //         break;
+    //     case 4:
+    //         echo "<img src='baraja/Oros/" +"$rand4"+".jpg'>";
+    //         break;
+    // }
 ?>
